37a8b67dbaf2aea11ed76649b5d106888269eaf9
Mikhail <mikhail.sapuzhak@gmail.com>
2020-04-12 12:02:20 +0300

    Updates

---
+++ precache-manifest.fbf8bba0a5c8a46d75ff2b7af2ba6ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c232082cec7fbcc04e129aad84adeb6f",
+    "url": "/react-notes-app/index.html"
+  },
+  {
+    "revision": "e8cc7408299eec0c4130",
+    "url": "/react-notes-app/static/css/main.958bd742.chunk.css"
+  },
+  {
+    "revision": "64648daaad3be030c2a6",
+    "url": "/react-notes-app/static/js/2.dbadd072.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-notes-app/static/js/2.dbadd072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cc7408299eec0c4130",
+    "url": "/react-notes-app/static/js/main.ef1c2a9b.chunk.js"
+  },
+  {
+    "revision": "14f133b799d05dcae8ca",
+    "url": "/react-notes-app/static/js/runtime-main.3b8cdce8.js"
+  }
+]);
